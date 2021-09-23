2f77b1ec688bc6a68f659ecdf7cfb784f57552f1
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-23 03:02:35 +0000

    initial init for word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,61 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    let obj = {};
+    for (let i = 0; i < letters.length; i++) {
+      const row = letters[i];
+      for (let j = 0; j < row.length; j++) {
+        if (obj[j]) {
+          obj[j].push(row[j]);
+        } else {
+          obj[j] = [row[j]];
+        }
+      }
     }
-}
-
-module.exports = wordSearch
+  
+    const verticalJoin = Object.values(obj).map((ls) => ls.join(""));
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+  
+    for (l of horizontalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    for (l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+  
+    return false;
+  };
+  
+  wordSearch(
+    [
+      ["F", "R", "A", "N", "K", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "T", "E", "V", "R", "G"],
+      ["W", "H", "C", "S", "Y", "E", "R", "L"],
+      ["B", "F", "R", "E", "N", "E", "Y", "B"],
+      ["U", "B", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ],
+    "FRANK"
+  );
+  
+  wordSearch(
+    [
+      ["S", "H", "C", "F", "Q", "U", "A", "L"],
+      ["E", "E", "I", "N", "F", "E", "L", "D"],
+      ["I", "L", "C", "F", "Q", "U", "A", "L"],
+      ["N", "L", "J", "T", "E", "V", "R", "G"],
+      ["F", "O", "C", "S", "Y", "E", "R", "L"],
+      ["E", "W", "R", "E", "N", "E", "Y", "B"],
+      ["L", "O", "T", "W", "A", "P", "A", "I"],
+      ["D", "R", "C", "A", "K", "U", "A", "S"],
+      ["E", "L", "K", "F", "Q", "U", "A", "L"],
+    ],
+    "SEINFELD"
+  );
+  module.exports = wordSearch;
+  
